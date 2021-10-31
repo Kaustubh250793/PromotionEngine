ee01e6f2c8e73d0d07feda6cc8879d099a039115
Kaustubh Ingale <k.ingale@cubris.dk>
2021-10-31 13:40:01 +0100

    Messed up with project files and reordering

--- PromotionEngineClassLibrary.UnitTests/Properties/AssemblyInfo.cs
+++ PromotionEngineClassLibrary.UnitTests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("PromotionEngineService.UnitTests")]
+[assembly: AssemblyTitle("PromotionEngineClassLibrary.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PromotionEngineService.UnitTests")]
+[assembly: AssemblyProduct("PromotionEngineClassLibrary.UnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("b2c695b9-e0b3-478a-bf1e-16d9857a7273")]
+[assembly: Guid("a152cf08-00f6-42ee-8089-6ae48087bc8b")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
